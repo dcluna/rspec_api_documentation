beab65ddae30afa6d50516fd1429b1a2bcce6081
Daniel Luna <dancluna@gmail.com>
2018-08-01 21:26:30 -0300

    Grouping examples with action_name (in addition to route_uri and route_optionals)
    Mainly so tools like APITransformer can process groups correctly.

--- lib/rspec_api_documentation/views/api_blueprint_index.rb
+++ lib/rspec_api_documentation/views/api_blueprint_index.rb
@@ -8,7 +8,7 @@ module RspecApiDocumentation
 
       def sections
         super.map do |section|
-          routes = section[:examples].group_by { |e| "#{e.route_uri}#{e.route_optionals}" }.map do |route, examples|
+          routes   = section[:examples].group_by { |e| "#{e.route_uri}#{e.route_optionals}#{e.action_name}" }.map do |_route, examples|
             attrs  = fields(:attributes, examples)
             params = fields(:parameters, examples)
 
